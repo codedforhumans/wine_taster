287591c6655c228543af4e50f3140c3c7c5ea091
Saruul Amarbayar <a.saruuld@gmail.com>
2023-09-26 17:07:24 -0700

    Changed debug

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ app.layout = layout # layout_login
 
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8050, debug=True)
+    app.run_server(host="0.0.0.0", port=8050, debug=False)
